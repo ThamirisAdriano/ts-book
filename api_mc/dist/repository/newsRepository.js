6b2632ee0bde4733312df12692f2ab50facdc469
Thamiris Adriano <adrianosthamiris@gmail.com>
2023-07-06 06:42:44 -0300

    merge

--- api_mc/dist/repository/newsRepository.js
+++ api_mc/dist/repository/newsRepository.js
@@ -5,15 +5,4 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.NewsRepository = void 0;
 const mongoose_1 = __importDefault(require("mongoose"));
-const NewsSchema = new mongoose_1.default.Schema({
-    titulo: { type: String },
-    chapeu: { type: String },
-    texto: { type: String },
-    autor: { type: String },
-    imagem: { type: String },
-    dataPublicacao: { type: Date },
-    tags: { type: String },
-    link: { type: String },
-    ativo: { type: Boolean }
-});
-exports.NewsRepository = mongoose_1.default.model("news", NewsSchema);
+exports.NewsRepository = mongoose_1.default.model("news");
